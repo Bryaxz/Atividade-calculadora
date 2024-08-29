88a206a28c6776b0cbc3398544e5476edaa77a92
Bryan <142256559+Bryaxz@users.noreply.github.com>
2024-08-29 16:46:44 -0300

    Add files via upload

---
+++ Atv calculadora/script.js
@@ -0,0 +1,63 @@
+t1.addEventListener("click", function(){ 
+    bttel.value += t1.value;
+    });
+    
+    t2.addEventListener("click", function(){ 
+    bttel.value += t2.value;
+    });
+    
+    t3.addEventListener("click", function(){ 
+    bttel.value += t3.value;
+    });
+    
+    t4.addEventListener("click", function(){
+    bttel.value += t4.value;
+    });
+    
+    t5.addEventListener("click", function(){
+    bttel.value += t5.value;
+    });
+    
+    t6.addEventListener("click", function(){
+    bttel.value += t6.value;
+    });
+    
+    t7.addEventListener("click", function(){
+    bttel.value += t7.value;
+    });
+    
+    t8.addEventListener("click", function(){
+    bttel.value += t8.value;
+    });
+    
+    t9.addEventListener("click", function(){
+    bttel.value += t9.value;
+    });
+    
+    t10.addEventListener("click", function(){
+    bttel.value += t10.value;
+    });
+    
+    t11.addEventListener("click", function(){
+    bttel.value += t11.value;
+    });
+    
+    t12.addEventListener("click", function(){
+    bttel.value += t12.value;
+    });
+    
+    t13.addEventListener("click", function(){
+    bttel.value += t13.value;
+    });
+    
+    t14.addEventListener("click", function(){
+    bttel.value += t14.value;
+    });
+    
+    t15.addEventListener("click", function(){
+    bttel.value += t15.value;
+    });
+    
+    t16.addEventListener("click", function(){
+    bttel.value = eval(bttel.value);
+    });
